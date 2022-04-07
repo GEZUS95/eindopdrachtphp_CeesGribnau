dfb8e3c1c13205070de74d13776b3c67f4e238a7
GEZUS95 <ceesgribnau@hotmail.com>
2022-04-07 12:06:03 +0200

    changed the models like the vue project

--- app/models/review.php
+++ app/models/review.php
@@ -1,88 +1,13 @@
 <?php
+namespace Models;
 
-class review
-{
-    private int $company_id;
-    private int $user_id;
-    private int $stars;
-    private string $description;
+class Review {
 
-    /**
-     * @param int $company_id
-     * @param int $user_id
-     * @param int $stars
-     */
-    public function __construct(int $company_id, int $user_id, int $stars)
-    {
-        $this->company_id = $company_id;
-        $this->user_id = $user_id;
-        $this->stars = $stars;
-    }
-
-    /**
-     * @return int
-     */
-    public function getCompanyId(): int
-    {
-        return $this->company_id;
-    }
-
-    /**
-     * @param int $company_id
-     */
-    public function setCompanyId(int $company_id): void
-    {
-        $this->company_id = $company_id;
-    }
-
-    /**
-     * @return int
-     */
-    public function getUserId(): int
-    {
-        return $this->user_id;
-    }
-
-    /**
-     * @param int $user_id
-     */
-    public function setUserId(int $user_id): void
-    {
-        $this->user_id = $user_id;
-    }
-
-    /**
-     * @return int
-     */
-    public function getStars(): int
-    {
-        return $this->stars;
-    }
-
-    /**
-     * @param int $stars
-     */
-    public function setStars(int $stars): void
-    {
-        $this->stars = $stars;
-    }
-
-    /**
-     * @return string
-     */
-    public function getDescription(): string
-    {
-        return $this->description;
-    }
-
-    /**
-     * @param string $description
-     */
-    public function setDescription(string $description): void
-    {
-        $this->description = $description;
-    }
-
-
+    public int $companyId;
+    public int $userId;
+    public string $title;
+    public string $description;
+    public int $rating;
+    public string $reaction;
 
 }
