d0863a223c00a2bbbd38ae4e8180766656e110e4
GEZUS95 <ceesgribnau@hotmail.com>
2022-04-08 17:26:52 +0200

    added company overview on homepage

--- app/controllers/homecontroller.php
+++ app/controllers/homecontroller.php
@@ -2,20 +2,26 @@
 
 session_start();
 
-require '../helpers/session_helper.php';
+require __DIR__ . '/../helpers/session_helper.php';
+require __DIR__ . '/../repositories/repository.php';
+require __DIR__ . '/../models/company.php';
+require __DIR__ . '/../services/companyservice.php';
 
 class homecontroller
 {
     protected sessionHelper $sesHelp;
+    private CompanyService $companyService;
 
     public function __construct()
     {
+        $this->companyService = new CompanyService();
         $this->sesHelp = new sessionHelper();
     }
 
 
     public function index()
     {
+        $companys = $this->companyService->getAll();
         require __DIR__ . '/../views/home/index.php';
     }
 
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -2,8 +2,20 @@
 
 include __DIR__ . '/../header.php';
 
-echo "<h1>Homepage!</h1>";
+echo '<h1>Homepage!</h1>';
 
 $this->sesHelp->message();
+
+foreach ($companys as $company) {
+    echo "<div class='card' style='width: 22rem;'>";
+    if (isset($company->logo) && $company->logo != '') echo "<img class='card-img-top' src='$company->logo' alt='$company->name'>";
+    echo "
+<div class='card-body'>
+    <h5 class='card-title'>$company->name</h5>
+    <p class='card-text'>$company->description</p>
+    <a href='/review/create' class='btn btn-primary'>Place Review</a>
+  </div>
+</div>";
+}
 
 include __DIR__ . '/../footer.php';
