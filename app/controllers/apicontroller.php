a355310c101a006b3f4acf892c125d0140108434
GEZUS95 <ceesgribnau@hotmail.com>
2022-04-08 18:13:18 +0200

    Added api endpoint

---
+++ app/controllers/apicontroller.php
@@ -0,0 +1,58 @@
+<?php
+
+require __DIR__ . '/../models/company.php';
+require __DIR__ . '/../models/review.php';
+require __DIR__ . '/../models/user.php';
+require __DIR__ . '/../repositories/repository.php';
+require __DIR__ . '/../services/companyservice.php';
+require __DIR__ . '/../services/userservice.php';
+require __DIR__ . '/../services/reviewservice.php';
+
+
+
+class apicontroller
+{
+
+    private CompanyService $companyService;
+    private ReviewService $reviewService;
+    private $userService;
+
+
+    public function __construct()
+    {
+        $this->companyService = new companyservice();
+        $this->reviewService = new ReviewService();
+        $this->userService = new userService();
+    }
+
+    public function reviews()
+    {
+        foreach ($this->reviewService->getAll() as $row) {
+        $array[] = $row;
+        }
+        $json = json_encode($array);
+        print($json);
+        return $json;
+    }
+
+    public function companys()
+    {
+        foreach ($this->companyService->getAll() as $row)
+        {
+            $array[] = $row;
+        }
+        $json = json_encode($array);
+        print($json);
+        return $json;
+    }
+    public function users()
+    {
+        foreach ($this->userService->getAll() as $row)
+        {
+            $array[] = $row;
+        }
+        $json = json_encode($array);
+        print($json);
+        return $json;
+    }
+}
